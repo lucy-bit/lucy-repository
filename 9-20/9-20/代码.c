ba822b73c285f42d0425107d438a5f003abfa716
lucy-bit <307954897@qq.com>
2019-09-20 23:12:17 +0800

    代码

---
+++ 9-20/9-20/代码.c
@@ -0,0 +1,26 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+//int main()
+//{
+//	char arr[10];
+//	scanf("%s", arr);
+//	printf("%s", arr);
+//	return 0;
+//}//用%s 中间别带空格
+
+int main()
+{
+	int a = 0;
+#if 0
+	if (a = 1)
+	{
+
+	}
+#endif
+	while (a = 1)
+	{
+		;
+	}
+	
+	return 0;
+}
