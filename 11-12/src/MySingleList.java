6ca61eb9e69275d5bed98dd740f2a311da2d6adb
lucy-bit <307954897@qq.com>
2019-11-12 23:34:24 +0800

    最后三道题代码及对之前链表习题的练习

---
+++ 11-12/src/MySingleList.java
@@ -0,0 +1,352 @@
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ * User: 86187
+ * Date: 2019 -11 -12
+ * Time: 16:40
+ */
+class ListNode {
+    public int data;
+    public ListNode next;
+    public ListNode(int data) {
+        this.data = data;
+        this.next = null;
+    }
+}
+class MySingleList {
+
+    public ListNode head;//标志头
+
+    public MySingleList() {
+        this.head = null;
+    }
+}
+    //头插法
+    public void addFirst(int data){
+        ListNode node = new ListNode(data);
+        if(this.head == null) {
+            this.head = node;
+        }else {
+            node.next = this.head;
+            this.head = node;
+        }
+    }
+
+    //尾插法
+    public void addLast(int data) {
+        ListNode node = new ListNode(data);
+        ListNode cur = this.head;
+        //0、判断是否是第一次插入
+        if(this.head == null) {
+            this.head = node;
+        }else {
+            //1、找尾巴
+            while (cur.next != null) {
+                cur = cur.next;
+            }
+            //2、进行插入
+            cur.next = node;
+        }
+    }
+    //打印单链表数据
+    public void display(){
+        if(this.head == null) {
+            return;
+        }
+        ListNode cur = this.head;
+        while (cur != null) {
+            System.out.print(cur.data+" ");
+            cur = cur.next;
+        }
+        System.out.println();
+    }
+//反转单链表
+public ListNode reverseList() {
+    ListNode cur = this.head;
+    ListNode prev = null;
+    ListNode newHead = null;
+    if(this.head == null) {
+        return null;
+    }
+    while(cur != null) {
+        ListNode curNext = cur.next;
+        if(curNext == null) {
+            newHead = cur;
+        }
+        cur.next = prev;
+        prev = cur;
+        cur = curNext;
+    }
+    return newHead;
+}
+//找到链表当中倒数第k个数
+public ListNode findKthToTail(int k) {
+    //判断k的合法性
+    if(k <= 0) {
+        return null;
+    }
+    //让fast先向后走k-1步，然后fast和slow同步走
+    ListNode fast = this.head;
+    ListNode slow = this.head;
+    while(k-1 < 0) {
+        if(fast.next != null) {
+            fast = fast.next;
+            k--;
+        }else {
+            System.out.println("没有这个节点");
+            return null;
+        }
+    }
+    while(fast.next != null) {
+        fast = fast.next;
+        slow = slow.next;
+    }
+    return slow;
+}
+//以x为基准分割链表
+public ListNode partition(int x) {
+    ListNode bs = null;
+    ListNode be = null;
+    ListNode as = null;
+    ListNode ae = null;
+    ListNode cur = this.head;
+    while(cur != null) {
+        if(cur.data < x) {
+            //判断是否为第一次插入
+            if(bs == null) {
+                bs = cur;
+                be = cur;
+            }else {
+                be.next = cur;
+                be = be.next;
+            }
+        }else {
+            if(as == null) {
+                as = cur;
+                ae = cur;
+            }else {
+                ae.next = cur;
+                ae = ae.next;
+            }
+        }
+        cur = cur.next;
+    }
+    if(bs == null) {
+        return as;
+    }
+    be.next = as;
+    //避免出现死循环；
+    if(as != null) {
+        ae.next = null;
+    }
+    return bs;
+}
+//7. 在一个排序的链表中，存在重复的结点，请删除该链表中重复的结点，重复的结点不保留，返回链表头指针
+public ListNode deleteDuplication() {
+    ListNode cur = this.head;
+    ListNode newHead = new ListNode(-1);
+    ListNode tmp = newHead;
+    if(this.head == null) {
+        return null;
+    }
+    while(cur != null) {
+        if(cur.next != null && cur.data == cur.next.data) {
+            while(cur.next != null && cur.data == cur.next.data) {
+                cur = cur.next;
+            }
+            cur = cur.next;
+        }else {
+            tmp.next = cur;
+            tmp = tmp.next;
+            cur = cur.next;
+        }
+    }
+    tmp.next = null;
+    return newHead;
+}
+
+//8. 链表的回文结构
+public boolean chkPalindrome() {
+    //先判断
+    if(this.head == null) {
+        return false;
+    }
+    if(this.head.next == null) {
+        return true;
+    }
+    ListNode fast = this.head;
+    ListNode slow = this.head;
+    //找到中间节点
+    while(fast != null && fast.next != null) {
+        fast = fast.next.next;
+        slow = slow.next;
+    }
+    //进行反转操作
+    ListNode cur = slow.next;
+    while(cur != null) {
+        ListNode curNext = cur.next;
+        cur.next = slow;
+        slow = slow.next;
+        cur = curNext;
+    }
+    //让slow向前走，head往后走
+    while(slow != this.head) {
+        if(slow.data != this.head.data) {
+            return false;
+        }
+        //偶数的情况
+        if(this.head.next == slow) {
+            return true;
+        }
+        slow = slow.next;
+        this.head = this.head.next;
+    }
+    return true;
+}
+
+//10. 给定一个链表，判断链表中是否有环
+public boolean hasCycle() {
+    ListNode fast = this.head;
+    ListNode slow = this.head;
+    while(fast != null && fast.next != null) {
+        fast = fast.next.next;
+        slow = slow.next;
+        if(slow.data == fast.data) {
+            return true;
+        }
+    }
+    return false;
+}
+
+//11. 给定一个链表，返回链表开始入环的第一个节点。
+public ListNode detectCycle() {
+    ListNode fast = this.head;
+    ListNode slow = this.head;
+    while(fast != null && fast.next != null) {
+        fast = fast.next.next;
+        slow = slow.next;
+        if (slow.data == fast.data) {
+            break;
+        }
+    }
+    if(fast == null || fast.next == null) {
+        return null;
+    }
+    slow = this.head;
+    while(slow != fast) {
+        slow = slow.next;
+        fast = fast.next;
+    }
+    return slow;
+}
+//再写一遍链表的回文结构
+/*public boolean chkPalindrome() {
+    if(this.head == null) {
+        return false;
+    }
+    if(this.head.next == null) {
+        return true;
+    }
+    ListNode fast = this.head;
+    ListNode slow = this.head;
+    while(fast != null && fast.next != null) {
+        fast = fast.next.next;
+        slow = slow.next;
+    }
+    //反转
+    ListNode cur = slow.next;
+    while(cur != null) {
+        ListNode curNext = cur.next;
+        cur.next = slow;
+        slow = slow.next;
+        cur = curNext;
+    }
+    while(slow != this.head) {
+        if(slow.data != this.head.data) {
+            return false;
+        }
+        if(this.head.next == slow) {
+            return true;
+        }
+        slow = slow.next;
+        this.head = this.head.next;
+    }
+    return true;
+}*/
+
+//以x为基准分割链表
+public ListNode partition(int x) {
+    ListNode bs = null;
+    ListNode be = null;
+    ListNode as = null;
+    ListNode ae = null;
+    ListNode cur = this.head;
+    while(cur != null) {
+        if(cur.data < x) {
+            if(bs == null) {
+                bs = cur;
+                be =cur;
+            }else {
+                be.next = cur;
+                be = be.next;
+            }
+        }else {
+
+        }
+        cur = cur.next;
+    }
+    if(bs == null) {
+        return as;
+    }
+    be.next = as;
+    if(as != null) {
+        ae.next = null;
+    }
+    return bs;
+}
+
+//再写一遍倒数第k个节点
+/*public ListNode findKthToTail(int k) {
+    if(k <= 0) {
+        return null;
+    }
+    ListNode fast = this.head;
+    ListNode slow = this.head;
+    while(k-1 < 0) {
+        if(fast.next != null) {
+            fast = fast.next;
+            k--;
+        }else {
+            System.out.println("没有这个节点");
+            return null;
+        }
+    }
+    while(fast.next != null) {
+        fast = fast.next;
+        slow = slow.next;
+    }
+    return slow;
+}*/
+
+//反转单链表
+/*public ListNode reverseList() {
+    ListNode cur = this.head;
+    ListNode prev = null;
+    ListNode newHead = null;
+    while(cur != null) {
+        ListNode curNext = cur.next;
+        if(curNext == null) {
+            newHead = cur;
+        }
+        cur.next = prev;
+        prev = cur;
+        cur = curNext;
+    }
+    return newHead;
+
+}*/
+
+//5. 将两个有序链表合并为一个新的有序链表并返回。新链表是通过拼接给定的两个链表的所有节点组成的
+
+class MySingleList {
+}
---
+++ 11-12/src/Testdemo.java
@@ -0,0 +1,144 @@
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ * User: 86187
+ * Date: 2019 -11 -12
+ * Time: 19:13
+ */
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ * User: GAOBO
+ * Date: 2019-11-03
+ * Time: 11:35
+ */
+ public class TestDemo {
+
+
+    public static ListNode mergeTwoLists(ListNode headA, ListNode headB) {
+        ListNode newHead = new ListNode(-1);
+        ListNode tmp = newHead;
+        while (headA != null && headB != null) {
+            if (headA.data < headB.data) {
+                tmp.next = headA;
+                headA = headA.next;
+                tmp = tmp.next;
+            } else {
+                tmp.next = headB;
+                headB = headB.next;
+                tmp = tmp.next;
+            }
+        }
+        if (headA == null) {
+            tmp.next = headB;
+        } else {
+            tmp.next = headA;
+        }
+        return newHead.next;
+    }
+
+
+    public void display2(ListNode newHead) {
+        if (newHead == null) {
+            return;
+        }
+        ListNode cur = newHead;
+        while (cur != null) {
+            System.out.print(cur.data + " ");
+            cur = cur.next;
+        }
+        System.out.println();
+    }
+
+    public ListNode getIntersectionNode(ListNode headA,ListNode headB) {
+        if(headA == null || headB == null) {
+            return null;
+        }
+        ListNode pL = headA;//始终让pL指向长的单链表
+        ListNode pS = headB;//指向短的
+        int lenA = 0;
+        int lenB = 0;
+        while(pL != null) {
+            pL = pL.next;
+            lenA++;
+        }
+        while(pS != null) {
+            pS = pS.next;
+            lenB++;
+        }
+        int len = lenA - lenB;
+        pL = headA;
+        pS = headB;
+        if(len < 0) {
+            pL = headB;
+            pS = headA;
+            len = lenB - lenA;
+        }
+       /* if(len == 0) {
+            return null;
+        }*/
+        for(int i = 0; i < len; i++) {
+            pL = pL.next;
+        }
+        while(pL != pS) {
+            pL = pL.next;
+            pS = pS.next;
+        }
+        return pL;
+}
+public static void creatCut(ListNode headA,ListNode headB) {
+        headA.next = headB.next.next;
+}
+    public static void main(String[] args) {
+        MySingleList mySingleList = new MySingleList();
+        MySingleList mySingleList2 = new MySingleList();
+        mySingleList.addLast(1);
+        mySingleList.addLast(2);
+        mySingleList.addLast(3);
+        mySingleList.addLast(10);
+        mySingleList2.addLast(4);
+        mySingleList2.addLast(5);
+        mySingleList2.addLast(6);
+        mySingleList2.addLast(8);
+        mySingleList.display();
+        mergeTwoLists(mySingleList.head,mySingleList2.head);
+
+       // mySingleList.creteLoop();
+
+        boolean flg = mySingleList.hasCycle();
+        System.out.println(flg);
+        /*ListNode newHead =  mySingleList.deleteDuplication();
+        mySingleList.display2(newHead);
+        ListNode newHead = mySingleList.partition(4);
+        mySingleList.display2(newHead);*/
+
+
+
+        /*ListNode node = mySingleList.middleNode();
+        System.out.println(node.data);*/
+       /* System.out.println("逆置");
+        ListNode newHead = mySingleList.reverseList();
+        mySingleList.display2(newHead);*/
+
+       /* mySingleList.clear();
+        System.out.println("fakfalls");
+*/
+       /*  mySingleList.removeAllKey(2);
+        mySingleList.display();*/
+       /*mySingleList.remove(99);
+        System.out.println("删除99");
+        mySingleList.display();//199 1 2 3 4 5 999
+        mySingleList.remove(999);
+        System.out.println("删除999");
+        mySingleList.display();//199 1 2 3 4 5
+        mySingleList.remove(2);
+        System.out.println("删除2");
+        mySingleList.display();//199 1 3 4 5*/
+
+       /* boolean flg = mySingleList.contains1(99);
+        System.out.println(flg);
+
+        ListNode node = mySingleList.contains2(500);
+        System.out.println(node.data);//5节点的地址*/
+    }
+}
