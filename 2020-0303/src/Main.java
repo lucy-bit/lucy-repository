a1e94a632aeec2ae43800c2f420ff7ea5a0daccc
lucy-bit <307954897@qq.com>
2020-03-03 23:07:37 +0800

    每日一练练习

---
+++ 2020-0303/src/Main.java
@@ -0,0 +1,14 @@
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ * User: 86187
+ * Date: 2020 -03 -03
+ * Time: 21:57
+ */
+public class Main {
+    public static void main(String[] args) {
+        String str = "abcdef";
+        System.out.println(str.substring(0,2));
+        System.out.println(str.substring(2,6));
+    }
+}
