ca0e259661acc0651126d443efe6e736d383736c
lucy-bit <307954897@qq.com>
2020-03-07 22:14:20 +0800

    ，，

---
+++ 2020-0307/src/Main.java
@@ -0,0 +1,14 @@
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ * User: 86187
+ * Date: 2020 -03 -07
+ * Time: 11:56
+ */
+public class Main {
+    public static void main(String[] args) {
+        String str1 = "abcdef";
+        char ch = 'a';
+        System.out.println(str1.contains("as"));
+    }
+}
