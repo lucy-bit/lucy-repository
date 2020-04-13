57bc24bb84af1bc8d69fb6f93ab93eaa92567a98
lucy-bit <307954897@qq.com>
2020-04-13 22:54:11 +0800

    更新

--- 2020-0322/src/Main.java
+++ 2020-0322/src/Main.java
@@ -7,7 +7,7 @@
  */
 import java.util.*;
 
-public class ListNode {
+/*public class ListNode {
     int val;
     ListNode next = null;
 
@@ -32,9 +32,17 @@ public class Plus {
         }
         return ret;
     }
-}
+}*/
 public class Main {
 
+        public static void main(String[] args) {
+            long t = System.currentTimeMillis();
+            Random r = new Random(t);
+            for(int i = 0; i < 5; i++) {
+                int ran1 = r.nextInt(100);
+                System.out.println(ran1);
+            }
+        }
     /*public static void main2(String[] args) {
         String s = "hello world\r\n";
         byte[] bytes = s.getBytes();
