1c3404ac4254a5d1a993fa574561818f85e1bd45
lucy-bit <307954897@qq.com>
2020-04-27 22:58:13 +0800

    servlet学习

---
+++ 网络-HTTP协议-Servlet的使用/src/com/peixinchen/FirstServlet.java
@@ -0,0 +1,18 @@
+package com.peixinchen;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class FirstServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html; charset=utf-8");
+        PrintWriter writer = resp.getWriter();
+        writer.println("<h1>我的第一个 Servlet</h1>");
+        writer.println("<p>其实没有那么难</p>");
+    }
+}
