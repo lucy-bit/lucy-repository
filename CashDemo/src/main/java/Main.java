ede77df96261cdb21be20ab213f6f819a5f454ba
lucy-bit <307954897@qq.com>
2020-05-12 22:12:53 +0800

    web收银台项目初步搭建

---
+++ CashDemo/src/main/java/Main.java
@@ -0,0 +1,14 @@
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ * User: 86187
+ * Date: 2020 -05 -12
+ * Time: 20:49
+ */
+public class Main {
+    public static void main(String[] args) {
+        Person person = new Person();
+        System.out.println(person.getAge());
+        System.out.println(person);
+    }
+}
---
+++ CashDemo/src/main/java/Person.java
@@ -0,0 +1,14 @@
+import lombok.Data;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ * User: 86187
+ * Date: 2020 -05 -12
+ * Time: 20:38
+ */
+@Data
+public class Person {
+    private String name;
+    private int age;
+}
