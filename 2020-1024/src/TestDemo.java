56ebdb32caf1c61e8010467df832c816884967c2
lucy-bit <307954897@qq.com>
2020-10-24 20:42:43 +0800

    优先级队列练习

---
+++ 2020-1024/src/TestDemo.java
@@ -0,0 +1,31 @@
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.PriorityQueue;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ * User: 86187
+ * Date: 2020 -10 -24
+ * Time: 16:19
+ */
+public class TestDemo {
+    public static void main(String[] args) {
+        //PriorityQueue 当中 最小的元素 优先级更高一些
+        PriorityQueue<Integer> priorityQueue = new PriorityQueue<>();//容量默认是11
+        PriorityQueue<Integer> priorityQueue2 = new PriorityQueue<>(14);//容量是14
+        priorityQueue.offer(4);
+        priorityQueue.offer(1);
+        priorityQueue.offer(8);
+        System.out.println(priorityQueue.size());
+        System.out.println(priorityQueue.peek());
+        ArrayList<Integer> arrayList = new ArrayList<>();
+        arrayList.add(1);
+        arrayList.add(12);
+        Iterator<Integer> iterator = arrayList.iterator();
+        System.out.println("===================================================");
+        while(iterator.hasNext()) {
+            System.out.print(iterator.next() + " ");
+        }
+    }
+}
